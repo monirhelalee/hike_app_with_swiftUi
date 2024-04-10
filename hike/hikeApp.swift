dbb7c47573950839df193073819c3b6f7e47999f
monir <mdmonirhelale@gmail.com>
2024-04-10 23:04:25 +0600

    Initial Commit

---
+++ hike/hikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikeApp.swift
+//  hike
+//
+//  Created by Monir Haider Helalee on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct hikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
