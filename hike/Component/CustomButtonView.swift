bd68860199af40b04e4cd4ac09951a78916c3fc2
monir <mdmonirhelale@gmail.com>
2024-04-15 16:19:52 +0600

    added header , main contant and  button in card

---
+++ hike/Component/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  hike
+//
+//  Created by Monir Haider Helalee on 15/4/24.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
