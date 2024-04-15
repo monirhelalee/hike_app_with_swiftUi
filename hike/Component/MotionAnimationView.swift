fb4ad47fafdcc174e8596ac23ba271d86d2eb194
monir <mdmonirhelale@gmail.com>
2024-04-15 23:10:42 +0600

    motion animation added

---
+++ hike/Component/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  hike
+//
+//  Created by Monir Haider Helalee on 15/4/24.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
