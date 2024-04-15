65c6795b1d7357b22fbf98495eead04ac5cd4004
monir <mdmonirhelale@gmail.com>
2024-04-15 21:25:17 +0600

    added fotter button

--- hike/Component/CardView.swift
+++ hike/Component/CardView.swift
@@ -8,6 +8,20 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - FUNCTIONS
+    func randomImage(){
+        repeat{
+            randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber
+        
+        imageNumber = randomNumber
+    }
+    
     var body: some View {
         // MARK: - CARD
         
@@ -62,12 +76,44 @@ struct CardView: View {
                         )
                     )
                     .frame(width: 256,height: 256)
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(
+                            .default,
+                            value: imageNumber
+                        )
                 }
                 
                 // MARK: - FOTTER
+                
+                Button{
+                    // ACTION:
+                    print("tap explore")
+                    randomImage()
+                    
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [
+                                    .customGreenLight,
+                                    .customGreenMedium],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .shadow(
+                            color: .black.opacity(0.25),
+                            radius: 0.25,
+                            x: 1,
+                            y: 2
+                        )
+                }
+                .buttonStyle(GradientButton())
+                
             }
         } //: CARD
         .frame(width: 320,height: 570)
--- hike/Utility/GradientButtonStyle.swift
+++ hike/Utility/GradientButtonStyle.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal,30)
+            .background(
+                // conditional statement with nil coalescing
+                // condition ? A : B
+                configuration.isPressed ?
+                // A: press the button
+                LinearGradient(
+                    colors: [
+                        .customGrayMedium,
+                        .customGrayLight
+                        ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                ):
+                // B: not press the button
+                
+                LinearGradient(
+                    colors: [
+                        .customGrayLight,
+                        .customGrayMedium],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            ).cornerRadius(40)
+    }
+}
