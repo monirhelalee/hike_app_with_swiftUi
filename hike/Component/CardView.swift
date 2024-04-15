b9016f99fa8d4b75432692b4857bfb772a3943b4
monir <mdmonirhelale@gmail.com>
2024-04-15 16:20:41 +0600

    Added Header and button in card

--- hike/Component/CardView.swift
+++ hike/Component/CardView.swift
@@ -13,21 +13,61 @@ struct CardView: View {
         
         ZStack {
             CustomBackgroundView()
-            ZStack{
-                Circle().fill(
-                    LinearGradient(
-                        colors: [
-                            Color("ColorIndigoMedium"),
-                            Color("ColorSalmonLight")
-                        ],
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
+            VStack {
+                // MARK: - HEARDER
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [
+                                    .customGrayLight, 
+                                        .customGrayMedium
+                                    ],
+                                               startPoint: .top,
+                                               endPoint: .bottom)
+                        )
+                        
+                        Spacer()
+                        
+                        Button{
+                            // ACTION: show a sheet
+                            print("button pressed")
+                        }label: {
+                            CustomButtonView()
+                        }
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
+                
+                // MARK: - MAIN CONTENT
+                
+                ZStack{
+                    Circle().fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLight")
+                            ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
                     )
-                )
-                .frame(width: 256,height: 256)
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                    .frame(width: 256,height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                // MARK: - FOTTER
             }
         } //: CARD
         .frame(width: 320,height: 570)
--- hike/Utility/ColorExtension.swift
+++ hike/Utility/ColorExtension.swift
@@ -12,7 +12,7 @@ extension Color {
     static let customGreenLight = Color("ColorGreenLight")
     static let customGreenMedium = Color("ColorGreenMedium")
     static let customGreenDark = Color("ColorGreenDark")
-    static let customGrayLidgt = Color("ColorGrayLight")
+    static let customGrayLight = Color("ColorGrayLight")
     static let customGrayMedium = Color("ColorGrayMedium")
     static let customIndigoMedium = Color("ColorIndigoMedium")
     static let customSalmonLight = Color("ColorSalmonLight")
