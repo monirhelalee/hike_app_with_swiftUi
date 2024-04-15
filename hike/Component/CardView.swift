7342012941e2cb6f0a081a8cc99168f7022be4da
monir <mdmonirhelale@gmail.com>
2024-04-15 15:23:14 +0600

    added contains and card

--- hike/Component/CardView.swift
+++ hike/Component/CardView.swift
@@ -9,22 +9,28 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        ZStack{
-            Circle().fill(
-                LinearGradient(
-                    colors: [
-                        Color("ColorIndigoMedium"),
-                        Color("ColorSalmonLight")
-                    ],
-                    startPoint: .topLeading,
-                    endPoint: .bottomTrailing
+        // MARK: - CARD
+        
+        ZStack {
+            CustomBackgroundView()
+            ZStack{
+                Circle().fill(
+                    LinearGradient(
+                        colors: [
+                            Color("ColorIndigoMedium"),
+                            Color("ColorSalmonLight")
+                        ],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
                 )
-            )
-            .frame(width: 256,height: 256)
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
-        }
+                .frame(width: 256,height: 256)
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320,height: 570)
     }
 }
 
