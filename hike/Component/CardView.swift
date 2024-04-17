e803f52bb15b64fada2a59d9fb0784210b05718b
monir <mdmonirhelale@gmail.com>
2024-04-17 13:13:25 +0600

    added settings sheet

--- hike/Component/CardView.swift
+++ hike/Component/CardView.swift
@@ -12,6 +12,7 @@ struct CardView: View {
     
     @State private var imageNumber: Int = 1
     @State private var randomNumber: Int = 1
+    @State private var isShowingSheet: Bool = false
     
     // MARK: - FUNCTIONS
     func randomImage(){
@@ -50,9 +51,16 @@ struct CardView: View {
                         Button{
                             // ACTION: show a sheet
                             print("button pressed")
+                            isShowingSheet.toggle()
                         }label: {
                             CustomButtonView()
                         }
+                        .sheet(isPresented: $isShowingSheet, content: {
+                            SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium,.large])
+                            
+                        })
                     }
                     
                     Text("Fun and enjoyable outdoor activity for friends and families.")
