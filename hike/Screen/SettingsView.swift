b0826fd63dd85acda40795904a8838f2e2edd1a7
monir <mdmonirhelale@gmail.com>
2024-04-17 13:13:02 +0600

    added settings sheet

---
+++ hike/Screen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  hike
+//
+//  Created by Monir Haider Helalee on 17/4/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
